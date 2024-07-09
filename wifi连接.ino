89c2727afa778e2612f478c9f61ec95b4bdea3f6
SrStontium <153499485+SrStontium@users.noreply.github.com>
2024-07-09 16:42:31 +0800

    Update wifi连接.ino

--- wifi连接.ino
+++ wifi连接.ino
@@ -1,15 +1,27 @@
 #include<WiFi.h>
 #include <bits/stdc++.h>
-const char* id="HUAWEI-H5SRCA";   //定义两个字符串指针常量
-const char* psw="Su092600";
+const char* id="YOUR_WIFI_NAME";   //定义两个字符串指针常量
+
+void waiting()
+{
+  for(;WiFi.status()!=WL_CONNECTED;)
+    {
+      Serial.print(".");
+      delay(500);
+    }
+}
+
+const char* psw="YOUR_PASS_WORD";
 
 void setup() {
   Serial.begin(115200);
   Serial.print("has started");
   WiFi.begin(id,psw);
-  while(WiFi.status()!=WL_CONNECTED){			//未连接上
+  while(WiFi.status()!=WL_CONNECTED)
+  {			//未连接上
     delay(500);
-    Serial.println("正在连接...");
+    Serial.println("正在连接");
+    waiting();
   }
   Serial.println("连接成功！");				//连接上
 }
